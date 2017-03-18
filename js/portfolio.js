c92a5002195b9868a585ebdbe4d57107c74838d9
Jessica Tran <kryx11@gmail.com>
2017-03-18 14:46:03 +1100

    add changes to mobile menu

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,17 +1,49 @@
 $(document).ready(function(){
-  $('#menu').on('click',function(){
-    $('.menuList').toggleClass('display');
+  //Pop up menu on phones
+  var menuOn = false;
+  $('#menu, .menuList').on('click',function(){
+    if( menuOn === false){
+      $('.menuList').addClass('display');
+      $('.menuList').removeClass('slideOutRight');
+      $('.menuList').toggleClass('slideInRight');
+      menuOn = true;
+    } else{
+      $('.menuList').toggleClass('slideInRight');
+      $('.menuList').toggleClass('slideOutRight');
+      menuOn = false;
+    }
   });
 
-
-  $('.menuList').on('click',function(){
-    $('.menuList').toggleClass('display');
-  });
-
+  //Highlight clicked nav link
   $('nav li a').on('click',function(){
     $('nav li a').removeClass('selectedNav');
     $(this).toggleClass('selectedNav');
   })
 
+  //ANIMATION
+  var $animation_elements = $('.animated');
+  var $window = $(window);
 
-})
+  function animate_on_scroll(){
+
+    var windowh = $window.height();
+    var windowt = $window.scrollTop();
+    var windowb= (windowt + windowh);
+
+    $.each($animation_elements, function(){
+      var $element = $(this);
+      var elementh = $element.outerHeight();
+      var elementt= $element.offset().top;
+      var elementb= (elementt + elementh);
+
+      if( (elementt <= windowb)){
+        $element.addClass('in-view');
+      } else {
+        $element.removeClass('in-view');
+      }
+    });
+  }
+
+  $window.on('scroll', animate_on_scroll);
+
+});
